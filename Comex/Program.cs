59906fe47d7775e252fdb43fa9bb8f648c3dba26
djherondhy <djherondhyoficial@gmail.com>
2024-07-10 09:54:46 -0400

    Adicionar arquivos de projeto.

---
+++ Comex/Program.cs
@@ -0,0 +1,16 @@
+﻿string mensagemBoasVindas = "Sejam Bem Vindos ao Comex";
+
+
+void ExibirMenu() {
+    Console.WriteLine(@"
+░█████╗░░█████╗░███╗░░░███╗███████╗██╗░░██╗
+██╔══██╗██╔══██╗████╗░████║██╔════╝╚██╗██╔╝
+██║░░╚═╝██║░░██║██╔████╔██║█████╗░░░╚███╔╝░
+██║░░██╗██║░░██║██║╚██╔╝██║██╔══╝░░░██╔██╗░
+╚█████╔╝╚█████╔╝██║░╚═╝░██║███████╗██╔╝╚██╗
+░╚════╝░░╚════╝░╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚═╝");
+
+    Console.WriteLine(mensagemBoasVindas);
+    Console.WriteLine("\n1 - Criar Produto.");
+    Console.WriteLine(" 2 - Listar Produtos.");
+}
