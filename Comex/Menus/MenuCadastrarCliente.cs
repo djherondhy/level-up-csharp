16ad90050d2263bb6fd01c8e9d24185ff4c96434
djherondhy <djherondhyoficial@gmail.com>
2024-08-09 09:20:27 -0400

    feat🌟: Funcionalidade de Cadastrar e Listar Clientes

---
+++ Comex/Menus/MenuCadastrarCliente.cs
@@ -0,0 +1,26 @@
+﻿using Comex.Models;
+
+namespace Comex.Menus; 
+internal class MenuCadastrarCliente:Menu {
+    
+    public MenuCadastrarCliente(List<Cliente> clientes) {
+        Clientes = clientes;
+    }
+
+    public List<Cliente> Clientes { get; set; }
+    public override void Executar(List<Produto> produtos) {
+        Console.Clear();
+        ExibirTitulo("Cadastrar Cliente");
+        Console.Write("Digite o Nome do Cliente:");
+        string nomeCliente = Console.ReadLine()!;
+        Console.Write("Digite o  CPF do Cliente:");
+        string cpfCliente = Console.ReadLine()!;
+
+        Cliente c  = new(nomeCliente) { CPF = cpfCliente };
+        Clientes.Add(c);
+
+        Console.WriteLine("\nCliente Adicionado com Sucesso!\n\n");
+
+        FinalizarOperacao();
+    }
+}
