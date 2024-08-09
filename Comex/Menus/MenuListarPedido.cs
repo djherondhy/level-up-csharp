3ee787dfdb95189bb44d762ac1a08abb031868d2
djherondhy <djherondhyoficial@gmail.com>
2024-08-09 09:21:55 -0400

    feat🌟: Criação das classes Item de Pedido e Pedido, e as funcionalidades de cadastro e listagem de pedidos

---
+++ Comex/Menus/MenuListarPedido.cs
@@ -0,0 +1,23 @@
+﻿using Comex.Models;
+
+namespace Comex.Menus; 
+internal class MenuListarPedido:Menu {
+    
+    public MenuListarPedido(List<Pedido> pedidos) {
+        Pedidos = pedidos;
+        
+    }
+    public List<Pedido> Pedidos { get; set; }
+    public override void Executar(List<Produto> produtos) {
+        Console.Clear();
+
+        Random random = new Random();
+
+        ExibirTitulo("Listar Pedido");
+        foreach(var pedidos in Pedidos) {
+            pedidos.detalharPedido();
+        }
+
+        FinalizarOperacao();
+    }
+}
---
+++ Comex/Models/ItemDePedido.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comex.Models; 
+internal class ItemDePedido {
+    public  Produto Produto { get; set; }
+
+    public int Quantidade { get; set; }
+    public decimal PrecoUnitario { get; set; }
+    public decimal Subtotal => Quantidade * PrecoUnitario;
+
+
+}
